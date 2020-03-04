fe36d21ddd8feb3d015088d25f7ca60dfe13d121
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-04 10:15:58 +0000

    Deploy anevis/datadog-to-terraform-js to github.com/anevis/datadog-to-terraform-js.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ac6a5fb757338d5c09d85f24741c9a6.js"
+  "./precache-manifest.388e1a57c8b7f9360b1244f9a3e0a690.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
