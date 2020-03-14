239a86147c4859309ad99fb82864f9606e08737a
anevis <asocial@nevisfamily.com>
2020-03-14 17:46:12 +1100

    Remove nested sections in build log

--- ci/indocker/_test.sh
+++ ci/indocker/_test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "${BLOCK_START_PREFIX}Formatting & Linting checks"
+echo "${BLOCK_START_PREFIX}Formatting.and.Linting"
 yarn format-check lint
-echo "${BLOCK_END_PREFIX}Formatting & Linting checks"
+echo "${BLOCK_END_PREFIX}Formatting.and.Linting"
 
-echo "${BLOCK_START_PREFIX}Running Unit Tests and Coverage"
+echo "${BLOCK_START_PREFIX}Unit.Tests.and.Coverage"
 yarn test-ci --coverage
-echo "${BLOCK_END_PREFIX}Running Unit Tests and Coverage"
+echo "${BLOCK_END_PREFIX}Unit.Tests.and.Coverage"
