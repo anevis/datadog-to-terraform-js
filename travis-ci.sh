39171e84b276dc9804ada9d39e4103a2e2551e9d
Antony Nevis <asocial@nevisfamily.com>
2020-12-22 16:01:16 +1100

    Encrypt docker password

--- travis-ci.sh
+++ travis-ci.sh
@@ -27,6 +27,28 @@ echo "Travis Pull Request=${TRAVIS_PULL_REQUEST}"
 echo "Travis Build Directory=${TRAVIS_BUILD_DIR}"
 
 echo "Docker authentication"
+# init key for pass
+gpg --batch --gen-key <<-EOF
+%echo Generating a standard key
+Key-Type: DSA
+Key-Length: 1024
+Subkey-Type: ELG-E
+Subkey-Length: 1024
+Name-Real: GHDL [travis-ci]
+Name-Email: ghdl@travis-ci
+Expire-Date: 0
+# Do a commit here, so that we can later print "done" :-)
+%commit
+%echo done
+EOF
+
+key=$(gpg --no-auto-check-trustdb --list-secret-keys | grep ^sec | cut -d/ -f2 | cut -d" " -f1)
+pass init $key
+
+curl -fsSL https://github.com/docker/docker-credential-helpers/releases/download/v0.6.0/docker-credential-pass-v0.6.0-amd64.tar.gz | tar x
+export PATH=$PATH:$(pwd)
+chmod + docker-credential-pass
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 ./ci.sh "$@"
